cec0c7bc9870d81e245f77be7724058250fe5ca0
Tao SAINT PAUL AMOURDAM <tsaint-p@paul-f5Br5s5.clusters.42paris.fr>
2023-09-07 16:17:18 +0200

    started with put_image & free at exit

--- fractol.h
+++ fractol.h
@@ -6,23 +6,20 @@
 /*   By: tsaint-p <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/04 14:29:50 by tsaint-p          #+#    #+#             */
-/*   Updated: 2023/09/07 10:44:34 by tsaint-p         ###   ########.fr       */
+/*   Updated: 2023/09/07 12:11:46 by tsaint-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FRACTOL_H
 # define FRACTOL_H
+# define MLX_ERROR 1
 # define WINDOW_WIDTH 1200
 # define WINDOW_HEIGHT 300
 
 # include "minilibx-linux/mlx.h"
+# include <X11/keysym.h>
+# include <X11/X.h>
 # include <stdlib.h>
-
-typedef	struct s_window
-{
-	void	*mlx_ptr;
-	void	*win_ptr;
-} t_window;
 
 typedef struct s_img
 {
@@ -32,6 +29,14 @@ typedef struct s_img
     int		line_len;
     int		endian;
 }	t_img;
+
+typedef	struct s_window
+{
+	void	*mlx_ptr;
+    t_img	img;
+	void	*win_ptr;
+} t_window;
+
 
 /*----------------init.c---------------------*/
 t_window	*init_window(void);
--- main.c
+++ main.c
@@ -6,89 +6,102 @@
 /*   By: tsaint-p <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/04 12:29:33 by tsaint-p          #+#    #+#             */
-/*   Updated: 2023/09/04 17:13:31 by tsaint-p         ###   ########.fr       */
+/*   Updated: 2023/09/07 15:22:04 by tsaint-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
-#include <stdio.h>
+#include "minilibx-linux/mlx.h"
 
-int	degrade_vertical(void)
+void	img_pix_put(t_img *img, int x, int y, int color)
 {
-	static unsigned char	r;
-	static unsigned char	g;
-	static unsigned char	b;
-	static int	indicator = 1;
+    char    *pixel;
+    int		i;
 
-	if (indicator == 1)
-	{
-		if (g == 0xff)
-			indicator = 2;
-		else
-			g++;
-	}
-	if (indicator == 2)
-	{
-		if (!r)
-			indicator = 3;
-		else
-			r--;
-	}
-	if (indicator == 3)
-	{
-		if (b == 0xff)
-			indicator = 4;
-		else
-			b++;
-	}
-	if (indicator == 4)
-	{
-		if (!g)
-			indicator = 5;
-		else
-			g--;
-	}
-	if (indicator == 5)
-	{
-		if (r == 0xff)
-			indicator = 6;
-		else
-			r++;
-	}
-	if (indicator == 6)
-	{
-		if (!b)
-			indicator = 1;
-		else
-			b--;
-	}
-	return (r * 65536 + g * 256 + b);
+    i = img->bpp - 8;
+    pixel = img->addr + (y * img->line_len + x * (img->bpp / 8));
+    while (i >= 0)
+    {
+        /* big endian, MSB is the leftmost bit */
+        if (img->endian != 0)
+            *pixel++ = (color >> i) & 0xFF;
+        /* little endian, LSB is the leftmost bit */
+        else
+            *pixel++ = (color >> (img->bpp - 8 - i)) & 0xFF;
+        i -= 8;
+    }
+}
+
+int	handle_no_event(void *window)
+{
+	if (!window)
+		return (1);
+    return (0);
+}
+
+int	handle_input(int keysym, t_window *data)
+{
+    if (keysym == XK_Escape)
+        mlx_destroy_window(data->mlx_ptr, data->win_ptr);
+    return (0);
+}
+
+#include <stdio.h>
+int	handle_keypress(int keysym, t_window *window)
+{
+    if (keysym == XK_Escape)
+        mlx_destroy_window(window->mlx_ptr, window->win_ptr);
+
+    printf("Keypress: %d\n", keysym);
+    return (0);
+}
+
+int	handle_keyrelease(int keysym, void *window)
+{
+	if (!window)
+		return (1);
+    printf("Keyrelease: %d\n", keysym);
+    return (0);
 }
 
 int	main(void)
 {
 	t_window	*window;
-	int			i;
-	int			j;
-	int			color;
+	int			x;
+	int			y;
 
 	window = init_window();
-	i = 0;
-	j = 0;
-	color = 0xff0000;
-	while (i < WINDOW_WIDTH)
+	if (!window)
+		return (MLX_ERROR);
+	window->img.mlx_img = mlx_new_image(window->mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT);
+	window->img.addr = mlx_get_data_addr(window->img.mlx_img, &(window->img.bpp), &(window->img.line_len), &(window->img.endian));
+	x = 0;
+	while (x < WINDOW_WIDTH)
 	{
-		j = 0;
-		color = degrade_vertical();
-		while (j < WINDOW_HEIGHT)
+		y = 0;
+		while (y < WINDOW_HEIGHT)
 		{
-			mlx_pixel_put(window->mlx_ptr, window->win_ptr, i, j, color);
-			j++;
+			if (y == x || x == WINDOW_WIDTH / 2 || y == WINDOW_HEIGHT / 2)
+			{
+				img_pix_put(&(window->img), x, y, 0x0FFFFFF);
+			}
+			y++;
 		}
-		i++;
+		x++;
 	}
+    mlx_put_image_to_window(window->mlx_ptr, window->win_ptr, window->img.mlx_img, 0, 0);
+
+    /* Setup hooks */ 
+    mlx_loop_hook(window->mlx_ptr, &handle_no_event, window);
+    mlx_key_hook(window->win_ptr, &handle_input, window);
+	mlx_hook(window->win_ptr, KeyPress, KeyPressMask, &handle_keypress, window); /* ADDED */
+    mlx_hook(window->win_ptr, KeyRelease, KeyReleaseMask, &handle_keyrelease, window); /* CHANGED */
+
 	mlx_loop(window->mlx_ptr);
-	mlx_destroy_window(window->mlx_ptr, window->win_ptr);
+
+	// exit and free
+    mlx_destroy_image(window->mlx_ptr, window->img.mlx_img);
     mlx_destroy_display(window->mlx_ptr);
 	free(window->mlx_ptr);
+	free(window);
 }
